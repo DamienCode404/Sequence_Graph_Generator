d886272f0017534d3ebdd574f5740bb110258b13
DamienCode404 <damien.rat44@gmail.com>
2023-09-21 20:39:40 +0200

    add heatmap generation and readme file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Sequence_Graph_Generator
+# Sequence Graph Generator from FASTA Files
+
+This project is a sequence graph generator for biological sequences from FASTA files. It allows users to visualize similarities between different sequences and generate graphs and heatmaps for comparative analysis.
+
+## Features
+
+- **FASTA File Loading**: The program allows users to load FASTA files containing biological sequences.
+
+- **Similarity Analysis**: It utilizes the Biopython library to compute similarities between sequences using global alignment.
+
+- **Graph Visualization**: Similarities between sequences are represented as graphs, where sequences are nodes and alignment scores are edge weights.
+
+- **Alignment Score Heatmap**: Alignment scores between selected sequences can be visualized as a heatmap.
+
+## How to Use
+
+1. Select a FASTA file using the "Open FASTA File" button.
+
+2. Sequences will be loaded and displayed. You can select the sequences you want to include in the graph or heatmap.
+
+3. Use the "Generate Graph" button to create a graph of similarities between the selected sequences.
+
+4. Use the "Generate Heatmap" button to create a heatmap of alignment scores between the selected sequences.
+
+## Dependencies
+
+This project uses the following libraries:
+
+- Biopython for FASTA file processing and alignment calculations.
+- NetworkX for graph creation and visualization.
+- Matplotlib for generating graphs.
+- CustomTkinter for a customized user interface.
+- Seaborn for heatmap creation.
--- desktop_app.py
+++ desktop_app.py
@@ -7,6 +7,15 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import customtkinter
+import seaborn as sns
+
+
+# Global variables
+fasta_file_path = ""
+sequences = []
+sequence_names = []
+alignments = []
+sequence_checkboxes = []
 
 
 def open_fasta_file():
@@ -19,22 +28,23 @@ def open_fasta_file():
 
 
 def reset_checkboxes():
-    for _, var in sequence_checkboxes:
-        var.set(0)
+    for checkbox, var in sequence_checkboxes:
+        checkbox.destroy()
+    sequence_checkboxes.clear()
 
 
 def load_sequences():
-    global sequences, sequence_names, alignments
-    sequences = []
-    sequence_names = []
-    alignments = []  # Reinitialize alignments list
-    reset_checkboxes()  # Reset checkboxes when loading new sequences
+    global sequences, sequence_names  # Utilisez les variables globales
+
+    sequences = []  # Réinitialisez la liste des séquences
+    sequence_names = []  # Réinitialisez la liste des noms de séquence
 
     try:
         fasta_sequences = SeqIO.parse(fasta_file_path, "fasta")
         for sequence in fasta_sequences:
             sequences.append(sequence.seq)
-            sequence_name = sequence.description.split(" ")[0]
+            sequence_name = sequence.description.split(
+                "|")[0]  # Obtenez le nom avant le caractère "|"
             sequence_names.append(sequence_name)
 
         for i in range(len(sequences)):
@@ -62,6 +72,10 @@ def update_sequence_checkboxes():
 
     # Enable or disable the "Generate Graph" button based on checkbox selection
     generate_button.configure(
+        state=tk.NORMAL if sequence_checkboxes else tk.DISABLED)
+
+    # Enable or disable the "Generate Heatmap" button based on whether sequences are loaded
+    heatmap_button.configure(
         state=tk.NORMAL if sequence_checkboxes else tk.DISABLED)
 
 
@@ -94,27 +108,78 @@ def generate_graph():
                 sequence_name_j = sequence_names[j]
                 G.add_edge(sequence_name_i, sequence_name_j, weight=score)
 
+    # Calculate the maximum alignment score for scaling the colorbar
+    max_alignment_score = max([d['weight'] for u, v, d in G.edges(data=True)])
+
     # Plot the graph
     pos = nx.spring_layout(G)
-    edge_labels = nx.get_edge_attributes(G, 'weight')
+    edge_labels = {(u, v): f"{d['weight']:.2f}" for u,
+                   v, d in G.edges(data=True)}
     edge_colors = [d['weight'] for u, v, d in G.edges(data=True)]
 
     plt.figure(figsize=(12, 8))
     nx.draw_networkx_nodes(
-        G, pos, node_size=3000, node_color='lightblue')
+        G, pos, node_size=3000, node_color='white')
     nx.draw_networkx_labels(G, pos, font_size=10, font_color='black')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=12)
     nx.draw_networkx_edges(G, pos, edge_color=edge_colors, edge_cmap=cm.RdYlBu)
+    # Specify vmin and vmax for the colorbar
     cbar = plt.colorbar(plt.cm.ScalarMappable(
-        cmap=cm.RdYlBu), label='Alignment Score')
+        cmap=cm.RdYlBu, norm=plt.Normalize(vmin=0, vmax=max_alignment_score)))
 
     plt.axis('off')
     plt.title('Alignment Score Graph')
     plt.show()
 
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+def generate_heatmap():
+    if not alignments:
+        messagebox.showinfo(
+            "Info", "Please load or reload sequences from a FASTA file.")
+        return
+
+    # Obtenez les indices des séquences sélectionnées à partir des cases à cocher
+    selected_sequence_indices = [i for i, (_, var) in enumerate(
+        sequence_checkboxes) if var.get() == 1]
+
+    if not selected_sequence_indices:
+        messagebox.showinfo(
+            "Info", "Please select at least one sequence to generate the heatmap.")
+        return
+
+    # Créez une liste pour stocker les séquences sélectionnées
+    selected_sequences = [sequences[i] for i in selected_sequence_indices]
+    # Créez une liste pour stocker les noms des séquences sélectionnées
+    selected_sequence_names = [sequence_names[i]
+                               for i in selected_sequence_indices]
+
+    # Créez une matrice carrée pour stocker les scores d'alignement entre les séquences sélectionnées
+    num_selected_sequences = len(selected_sequences)
+    scores_matrix = [
+        [0] * num_selected_sequences for _ in range(num_selected_sequences)]
+
+    for i in range(num_selected_sequences):
+        for j in range(i + 1, num_selected_sequences):
+            # Calculez les scores d'alignement entre les séquences sélectionnées
+            align = pairwise2.align.globalxx(
+                selected_sequences[i], selected_sequences[j], one_alignment_only=True)[0]
+            score = align.score
+            scores_matrix[i][j] = score
+            # Assurez-vous de remplir les deux côtés de la matrice
+            scores_matrix[j][i] = score
+
+    # Utilisez Seaborn pour créer un heatmap
+    sns.set()
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(scores_matrix, annot=True, fmt=".2f",
+                xticklabels=selected_sequence_names, yticklabels=selected_sequence_names)
+
+    plt.title("Alignment Score Heatmap")
+    plt.show()
+
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 # Create the main window
 root = customtkinter.CTk()
@@ -141,15 +206,14 @@ open_button = customtkinter.CTkButton(
     master=buttons_frame, text="Open FASTA File", command=open_fasta_file)
 generate_button = customtkinter.CTkButton(
     master=buttons_frame, text="Generate Graph", command=generate_graph, state=tk.DISABLED)
-open_button.pack(pady=12, padx=10, fill=tk.BOTH)
-generate_button.pack(pady=12, padx=10, fill=tk.BOTH)
+heatmap_button = customtkinter.CTkButton(
+    master=buttons_frame, text="Generate Heatmap", command=generate_heatmap, state=tk.DISABLED)
 
-# Global variables
-fasta_file_path = ""
-sequences = []
-sequence_names = []
-alignments = []
-sequence_checkboxes = []
+
+# Utilisez l'option side pour les mettre côte à côte
+open_button.pack(pady=12, padx=10, fill=tk.BOTH, side=tk.LEFT)
+generate_button.pack(pady=12, padx=10, fill=tk.BOTH, side=tk.LEFT)
+heatmap_button.pack(pady=12, padx=10, fill=tk.BOTH, side=tk.LEFT)
 
 # Start the main loop
 root.mainloop()
